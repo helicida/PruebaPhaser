29a5c8e84990af9071516648ece26d5d1fd99916
Sergi Barjola <sergibca@gmail.com>
2016-03-07 17:59:19 +0100

    Implementado el fondo a traves de capas (software Tiled)

--- main.js
+++ main.js
@@ -1,69 +1,60 @@
-/// <reference path="phaser/phaser.d.ts"/>
 var __extends = (this && this.__extends) || function (d, b) {
     for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
     function __() { this.constructor = d; }
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
+/// <reference path="phaser/phaser.d.ts"/>
 var mainState = (function (_super) {
     __extends(mainState, _super);
     function mainState() {
         _super.apply(this, arguments);
-        this.MAX_SPEED = 350; // pixels/second
+        // Constantes
+        this.VELOCIDAD_MAXIMA = 350; // pixels/second
         this.ACCELERATION = 250; // aceleración
         this.FUERZA_ROZAMIENTO = 100; // Aceleración negativa
     }
     mainState.prototype.preload = function () {
         _super.prototype.preload.call(this);
-        // Caragmos las imagenes del platillo
+        // Precargamos las imagenes del platillo
         this.load.image('ufo', 'assets/UFOLow.png');
         this.load.image('pickup', 'assets/PickupLow.png');
-        // Cargamos las imagenes de fondo
-        this.load.image('izquerdaArriba', 'assets/Background/BackgroundLow-0-0.png');
-        this.load.image('izquerdaCentro', 'assets/Background/BackgroundLow-0-1.png');
-        this.load.image('izquerdaAbajo', 'assets/Background/BackgroundLow-0-2.png');
-        this.load.image('centroArriba', 'assets/Background/BackgroundLow-1-0.png');
-        this.load.image('centroCentro', 'assets/Background/BackgroundLow-1-1.png');
-        this.load.image('centroAbajo', 'assets/Background/BackgroundLow-1-2.png');
-        this.load.image('derechaArriba', 'assets/Background/BackgroundLow-2-0.png');
-        this.load.image('derechaCentro', 'assets/Background/BackgroundLow-2-1.png');
-        this.load.image('derechaABajo', 'assets/Background/BackgroundLow-2-2.png');
+        // Precargamos el fondo a traves del tieleset
+        this.game.load.tilemap('tilemap', 'assets/Background.json', null, Phaser.Tilemap.TILED_JSON);
+        this.game.load.image('tiles', 'assets/BackgroundLow.png');
         // Declaramos el motor de físicas que vamos a usar
         this.physics.startSystem(Phaser.Physics.ARCADE);
     };
-    mainState.prototype.createWalls = function () {
-        this.paredes = this.add.group();
-        this.paredes.enableBody = true;
-        // Izquierda
-        var wall_izquerdaArriba = this.add.sprite(0, 0, 'izquerdaArriba', null, this.paredes);
-        var wall_izquerdaCentro = this.add.sprite(0, wall_izquerdaArriba.height, 'izquerdaCentro', null, this.paredes);
-        var wall_izquerdaAbajo = this.add.sprite(0, wall_izquerdaArriba.height + wall_izquerdaCentro.height, 'izquerdaAbajo', null, this.paredes);
-        // Centro
-        var wall_centroArriba = this.add.sprite(wall_izquerdaArriba.width, 0, 'centroArriba', null, this.paredes);
-        var wall_centroCentro = this.add.sprite(wall_izquerdaArriba.width, wall_centroArriba.height, 'centroCentro', null, this.paredes);
-        var wall_centroAbajo = this.add.sprite(wall_izquerdaArriba.width, wall_centroArriba.height + wall_centroCentro.height, 'centroAbajo', null, this.paredes);
-        // Derecha
-        var wall_derechaArriba = this.add.sprite(wall_izquerdaArriba.width + wall_centroCentro.width, 0, 'derechaArriba', null, this.paredes);
-        var wall_derechaCentro = this.add.sprite(wall_izquerdaArriba.width + wall_centroCentro.width, wall_derechaArriba.height, 'derechaCentro', null, this.paredes);
-        var wall_derechaAbajo = this.add.sprite(wall_izquerdaArriba.width + wall_centroCentro.width, wall_derechaArriba.height + wall_derechaCentro.height, 'derechaABajo', null, this.paredes);
-    };
     mainState.prototype.create = function () {
         _super.prototype.create.call(this);
-        var background;
-        this.createWalls();
+        // Cargamos las paredes
+        this.generarParedes();
+        // Sprite del platillo volante
         this.ufo = this.add.sprite(this.world.centerX, this.world.centerY, 'ufo');
+        this.ufo.anchor.setTo(0.5, 0.5);
         // Para el movimiento del platillo con las teclas
         this.cursor = this.input.keyboard.createCursorKeys();
         // Activamos la fisica
         this.physics.enable(this.ufo);
         // Le damos una aceleración
-        this.ufo.body.maxVelocity.setTo(this.MAX_SPEED, this.MAX_SPEED); // x, y
+        this.ufo.body.maxVelocity.setTo(this.VELOCIDAD_MAXIMA, this.VELOCIDAD_MAXIMA); // x, y
         // Fuerza de rozamiento
         this.ufo.body.drag.setTo(this.FUERZA_ROZAMIENTO, this.FUERZA_ROZAMIENTO); // x, y
         //velocidad maxima, colisiones y rebote
-        this.ufo.body.maxVelocity.setTo(this.MAX_SPEED, this.MAX_SPEED); // x, y
-        this.physics.arcade.collide(this.ufo, this.paredes);
+        this.ufo.body.maxVelocity.setTo(this.VELOCIDAD_MAXIMA, this.VELOCIDAD_MAXIMA); // x, y
+        this.ufo.body.collideWorldBounds = true;
         this.ufo.body.bounce.setTo(0.7);
     };
+    mainState.prototype.generarParedes = function () {
+        // Referencia al tilemap y al fondo
+        this.map = this.game.add.tilemap('tilemap');
+        this.map.addTilesetImage('BackgroundLow', 'tiles');
+        // Esto hace referencia a las capas
+        var background = this.map.createLayer('Centro');
+        this.paredes = this.map.createLayer('Limites');
+        // Auxiliar para la colision
+        this.map.setCollisionBetween(1, 100, true, 'Limites');
+    };
+    ;
     mainState.prototype.update = function () {
         _super.prototype.update.call(this);
         // Velocidad en el instante 0 del objeto
@@ -89,6 +80,7 @@ var mainState = (function (_super) {
 })(Phaser.State);
 var SimpleGame = (function () {
     function SimpleGame() {
+        // Tamaño del juego, etc
         this.game = new Phaser.Game(600, 600, Phaser.AUTO, 'gameDiv');
         this.game.state.add('main', mainState);
         this.game.state.start('main');
--- main.ts
+++ main.ts
@@ -1,65 +1,44 @@
 /// <reference path="phaser/phaser.d.ts"/>
-
 class mainState extends Phaser.State {
     game: Phaser.Game;
-    private ufo:Phaser.Sprite;
-    private cursor:Phaser.CursorKeys;
-    private paredes:Phaser.Group; // grupo con las paredes
 
-    private MAX_SPEED = 350;    // pixels/second
+    // Constantes
+    private VELOCIDAD_MAXIMA = 350;    // pixels/second
     private ACCELERATION = 250; // aceleración
     private FUERZA_ROZAMIENTO = 100; // Aceleración negativa
+
+    // Sprite elementos
+    private ufo:Phaser.Sprite;
+
+    // Otros recursos
+    private cursor:Phaser.CursorKeys;
+    private map:Phaser.Tilemap;
+    private paredes:Phaser.TilemapLayer;
 
     preload():void {
         super.preload();
 
-        // Caragmos las imagenes del platillo
+        // Precargamos las imagenes del platillo
         this.load.image('ufo', 'assets/UFOLow.png');
         this.load.image('pickup', 'assets/PickupLow.png');
 
-        // Cargamos las imagenes de fondo
-        this.load.image('izquerdaArriba', 'assets/Background/BackgroundLow-0-0.png')
-        this.load.image('izquerdaCentro', 'assets/Background/BackgroundLow-0-1.png')
-        this.load.image('izquerdaAbajo', 'assets/Background/BackgroundLow-0-2.png')
-        this.load.image('centroArriba', 'assets/Background/BackgroundLow-1-0.png')
-        this.load.image('centroCentro', 'assets/Background/BackgroundLow-1-1.png')
-        this.load.image('centroAbajo', 'assets/Background/BackgroundLow-1-2.png')
-        this.load.image('derechaArriba', 'assets/Background/BackgroundLow-2-0.png')
-        this.load.image('derechaCentro', 'assets/Background/BackgroundLow-2-1.png')
-        this.load.image('derechaABajo', 'assets/Background/BackgroundLow-2-2.png')
+        // Precargamos el fondo a traves del tieleset
+        this.game.load.tilemap('tilemap', 'assets/Background.json', null, Phaser.Tilemap.TILED_JSON);
+        this.game.load.image('tiles', 'assets/BackgroundLow.png');
 
         // Declaramos el motor de físicas que vamos a usar
         this.physics.startSystem(Phaser.Physics.ARCADE);
     }
 
-    private createWalls() {
-        this.paredes = this.add.group();
-        this.paredes.enableBody = true;
-
-        // Izquierda
-        var wall_izquerdaArriba = this.add.sprite(0, 0, 'izquerdaArriba', null, this.paredes);
-        var wall_izquerdaCentro = this.add.sprite(0, wall_izquerdaArriba.height, 'izquerdaCentro', null, this.paredes);
-        var wall_izquerdaAbajo = this.add.sprite(0, wall_izquerdaArriba.height + wall_izquerdaCentro.height, 'izquerdaAbajo', null, this.paredes);
-
-        // Centro
-        var wall_centroArriba = this.add.sprite(wall_izquerdaArriba.width, 0, 'centroArriba', null, this.paredes);
-        var wall_centroCentro = this.add.sprite(wall_izquerdaArriba.width, wall_centroArriba.height, 'centroCentro', null, this.paredes);
-        var wall_centroAbajo = this.add.sprite(wall_izquerdaArriba.width, wall_centroArriba.height + wall_centroCentro.height, 'centroAbajo', null, this.paredes);
-
-        // Derecha
-        var wall_derechaArriba = this.add.sprite(wall_izquerdaArriba.width + wall_centroCentro.width, 0 , 'derechaArriba', null, this.paredes);
-        var wall_derechaCentro = this.add.sprite(wall_izquerdaArriba.width + wall_centroCentro.width, wall_derechaArriba.height , 'derechaCentro', null, this.paredes);
-        var wall_derechaAbajo = this.add.sprite(wall_izquerdaArriba.width + wall_centroCentro.width, wall_derechaArriba.height + wall_derechaCentro.height , 'derechaABajo', null, this.paredes);
-    }
-
     create():void {
         super.create();
-        var background;
 
-        this.createWalls();
+        // Cargamos las paredes
+        this.generarParedes();
 
-
+        // Sprite del platillo volante
         this.ufo = this.add.sprite(this.world.centerX, this.world.centerY, 'ufo');
+        this.ufo.anchor.setTo(0.5, 0.5);
 
         // Para el movimiento del platillo con las teclas
         this.cursor = this.input.keyboard.createCursorKeys();
@@ -68,17 +47,31 @@ class mainState extends Phaser.State {
         this.physics.enable(this.ufo);
 
         // Le damos una aceleración
-        this.ufo.body.maxVelocity.setTo(this.MAX_SPEED, this.MAX_SPEED); // x, y
+        this.ufo.body.maxVelocity.setTo(this.VELOCIDAD_MAXIMA, this.VELOCIDAD_MAXIMA); // x, y
 
         // Fuerza de rozamiento
         this.ufo.body.drag.setTo(this.FUERZA_ROZAMIENTO, this.FUERZA_ROZAMIENTO); // x, y
 
         //velocidad maxima, colisiones y rebote
-        this.ufo.body.maxVelocity.setTo(this.MAX_SPEED, this.MAX_SPEED); // x, y
-        this.physics.arcade.collide(this.ufo, this.paredes)
+        this.ufo.body.maxVelocity.setTo(this.VELOCIDAD_MAXIMA, this.VELOCIDAD_MAXIMA); // x, y
+        this.ufo.body.collideWorldBounds = true;
         this.ufo.body.bounce.setTo(0.7);
 
     }
+
+    private generarParedes() {
+
+        // Referencia al tilemap y al fondo
+        this.map = this.game.add.tilemap('tilemap');
+        this.map.addTilesetImage('BackgroundLow', 'tiles');
+
+        // Esto hace referencia a las capas
+        var background = this.map.createLayer('Centro');
+        this.paredes = this.map.createLayer('Limites');
+
+        // Auxiliar para la colision
+        this.map.setCollisionBetween(1, 100, true, 'Limites');
+    };
 
     update():void {
         super.update();
@@ -91,7 +84,7 @@ class mainState extends Phaser.State {
         } else if (this.cursor.right.isDown) {
             this.ufo.body.acceleration.x = this.ACCELERATION;
 
-        // Movimientos en el eje Y
+            // Movimientos en el eje Y
         } else if (this.cursor.up.isDown) {
             this.ufo.body.acceleration.y = -this.ACCELERATION;
         } else if (this.cursor.down.isDown) {
@@ -103,12 +96,11 @@ class mainState extends Phaser.State {
     }
 }
 
-
-
 class SimpleGame {
     game:Phaser.Game;
 
     constructor() {
+        // Tamaño del juego, etc
         this.game = new Phaser.Game(600, 600, Phaser.AUTO, 'gameDiv');
 
         this.game.state.add('main', mainState);
