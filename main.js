e2bb2e867f941c8beed3ea7a85d41b2a02a01c8f
Sergi Barjola <sergibca@gmail.com>
2016-03-07 17:16:09 +0100

    Implementado el fondo por partes

--- main.js
+++ main.js
@@ -14,21 +14,43 @@ var mainState = (function (_super) {
     }
     mainState.prototype.preload = function () {
         _super.prototype.preload.call(this);
+        // Caragmos las imagenes del platillo
         this.load.image('ufo', 'assets/UFOLow.png');
         this.load.image('pickup', 'assets/PickupLow.png');
-        this.load.image('background', 'assets/BackgroundLow.png');
+        // Cargamos las imagenes de fondo
+        this.load.image('izquerdaArriba', 'assets/Background/BackgroundLow-0-0.png');
+        this.load.image('izquerdaCentro', 'assets/Background/BackgroundLow-0-1.png');
+        this.load.image('izquerdaAbajo', 'assets/Background/BackgroundLow-0-2.png');
+        this.load.image('centroArriba', 'assets/Background/BackgroundLow-1-0.png');
+        this.load.image('centroCentro', 'assets/Background/BackgroundLow-1-1.png');
+        this.load.image('centroAbajo', 'assets/Background/BackgroundLow-1-2.png');
+        this.load.image('derechaArriba', 'assets/Background/BackgroundLow-2-0.png');
+        this.load.image('derechaCentro', 'assets/Background/BackgroundLow-2-1.png');
+        this.load.image('derechaABajo', 'assets/Background/BackgroundLow-2-2.png');
         // Declaramos el motor de físicas que vamos a usar
         this.physics.startSystem(Phaser.Physics.ARCADE);
+    };
+    mainState.prototype.createWalls = function () {
+        this.paredes = this.add.group();
+        this.paredes.enableBody = true;
+        // Izquierda
+        var wall_izquerdaArriba = this.add.sprite(0, 0, 'izquerdaArriba', null, this.paredes);
+        var wall_izquerdaCentro = this.add.sprite(0, wall_izquerdaArriba.height, 'izquerdaCentro', null, this.paredes);
+        var wall_izquerdaAbajo = this.add.sprite(0, wall_izquerdaArriba.height + wall_izquerdaCentro.height, 'izquerdaAbajo', null, this.paredes);
+        // Centro
+        var wall_centroArriba = this.add.sprite(wall_izquerdaArriba.width, 0, 'centroArriba', null, this.paredes);
+        var wall_centroCentro = this.add.sprite(wall_izquerdaArriba.width, wall_centroArriba.height, 'centroCentro', null, this.paredes);
+        var wall_centroAbajo = this.add.sprite(wall_izquerdaArriba.width, wall_centroArriba.height + wall_centroCentro.height, 'centroAbajo', null, this.paredes);
+        // Derecha
+        var wall_derechaArriba = this.add.sprite(wall_izquerdaArriba.width + wall_centroCentro.width, 0, 'derechaArriba', null, this.paredes);
+        var wall_derechaCentro = this.add.sprite(wall_izquerdaArriba.width + wall_centroCentro.width, wall_derechaArriba.height, 'derechaCentro', null, this.paredes);
+        var wall_derechaAbajo = this.add.sprite(wall_izquerdaArriba.width + wall_centroCentro.width, wall_derechaArriba.height + wall_derechaCentro.height, 'derechaABajo', null, this.paredes);
     };
     mainState.prototype.create = function () {
         _super.prototype.create.call(this);
         var background;
-        background = this.add.sprite(0, 0, 'background');
-        var scale = this.world.height / background.height;
-        background.scale.setTo(scale, scale);
+        this.createWalls();
         this.ufo = this.add.sprite(this.world.centerX, this.world.centerY, 'ufo');
-        this.ufo.scale.setTo(scale - 0.05, scale - 0.05);
-        this.ufo.anchor.setTo(0.5, 0.5);
         // Para el movimiento del platillo con las teclas
         this.cursor = this.input.keyboard.createCursorKeys();
         // Activamos la fisica
@@ -39,8 +61,8 @@ var mainState = (function (_super) {
         this.ufo.body.drag.setTo(this.FUERZA_ROZAMIENTO, this.FUERZA_ROZAMIENTO); // x, y
         //velocidad maxima, colisiones y rebote
         this.ufo.body.maxVelocity.setTo(this.MAX_SPEED, this.MAX_SPEED); // x, y
-        this.ufo.body.collideWorldBounds = true;
-        this.ufo.body.bounce.setTo(2);
+        this.physics.arcade.collide(this.ufo, this.paredes);
+        this.ufo.body.bounce.setTo(0.7);
     };
     mainState.prototype.update = function () {
         _super.prototype.update.call(this);
--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ class mainState extends Phaser.State {
     game: Phaser.Game;
     private ufo:Phaser.Sprite;
     private cursor:Phaser.CursorKeys;
+    private paredes:Phaser.Group; // grupo con las paredes
 
     private MAX_SPEED = 350;    // pixels/second
     private ACCELERATION = 250; // aceleración
@@ -12,25 +13,53 @@ class mainState extends Phaser.State {
     preload():void {
         super.preload();
 
+        // Caragmos las imagenes del platillo
         this.load.image('ufo', 'assets/UFOLow.png');
         this.load.image('pickup', 'assets/PickupLow.png');
-        this.load.image('background', 'assets/BackgroundLow.png'    );
+
+        // Cargamos las imagenes de fondo
+        this.load.image('izquerdaArriba', 'assets/Background/BackgroundLow-0-0.png')
+        this.load.image('izquerdaCentro', 'assets/Background/BackgroundLow-0-1.png')
+        this.load.image('izquerdaAbajo', 'assets/Background/BackgroundLow-0-2.png')
+        this.load.image('centroArriba', 'assets/Background/BackgroundLow-1-0.png')
+        this.load.image('centroCentro', 'assets/Background/BackgroundLow-1-1.png')
+        this.load.image('centroAbajo', 'assets/Background/BackgroundLow-1-2.png')
+        this.load.image('derechaArriba', 'assets/Background/BackgroundLow-2-0.png')
+        this.load.image('derechaCentro', 'assets/Background/BackgroundLow-2-1.png')
+        this.load.image('derechaABajo', 'assets/Background/BackgroundLow-2-2.png')
 
         // Declaramos el motor de físicas que vamos a usar
         this.physics.startSystem(Phaser.Physics.ARCADE);
+    }
+
+    private createWalls() {
+        this.paredes = this.add.group();
+        this.paredes.enableBody = true;
+
+        // Izquierda
+        var wall_izquerdaArriba = this.add.sprite(0, 0, 'izquerdaArriba', null, this.paredes);
+        var wall_izquerdaCentro = this.add.sprite(0, wall_izquerdaArriba.height, 'izquerdaCentro', null, this.paredes);
+        var wall_izquerdaAbajo = this.add.sprite(0, wall_izquerdaArriba.height + wall_izquerdaCentro.height, 'izquerdaAbajo', null, this.paredes);
+
+        // Centro
+        var wall_centroArriba = this.add.sprite(wall_izquerdaArriba.width, 0, 'centroArriba', null, this.paredes);
+        var wall_centroCentro = this.add.sprite(wall_izquerdaArriba.width, wall_centroArriba.height, 'centroCentro', null, this.paredes);
+        var wall_centroAbajo = this.add.sprite(wall_izquerdaArriba.width, wall_centroArriba.height + wall_centroCentro.height, 'centroAbajo', null, this.paredes);
+
+        // Derecha
+        var wall_derechaArriba = this.add.sprite(wall_izquerdaArriba.width + wall_centroCentro.width, 0 , 'derechaArriba', null, this.paredes);
+        var wall_derechaCentro = this.add.sprite(wall_izquerdaArriba.width + wall_centroCentro.width, wall_derechaArriba.height , 'derechaCentro', null, this.paredes);
+        var wall_derechaAbajo = this.add.sprite(wall_izquerdaArriba.width + wall_centroCentro.width, wall_derechaArriba.height + wall_derechaCentro.height , 'derechaABajo', null, this.paredes);
     }
 
     create():void {
         super.create();
         var background;
 
-        background = this.add.sprite(0, 0, 'background');
-        var scale = this.world.height / background.height;
-        background.scale.setTo(scale, scale);
+        this.createWalls();
+
 
         this.ufo = this.add.sprite(this.world.centerX, this.world.centerY, 'ufo');
-        this.ufo.scale.setTo(scale - 0.05, scale - 0.05);
-        this.ufo.anchor.setTo(0.5, 0.5);
 
         // Para el movimiento del platillo con las teclas
         this.cursor = this.input.keyboard.createCursorKeys();
@@ -46,8 +75,8 @@ class mainState extends Phaser.State {
 
         //velocidad maxima, colisiones y rebote
         this.ufo.body.maxVelocity.setTo(this.MAX_SPEED, this.MAX_SPEED); // x, y
-        this.ufo.body.collideWorldBounds = true;
-        this.ufo.body.bounce.setTo(2);
+        this.physics.arcade.collide(this.ufo, this.paredes)
+        this.ufo.body.bounce.setTo(0.7);
 
     }
 
@@ -71,9 +100,10 @@ class mainState extends Phaser.State {
             this.ufo.body.acceleration.x = 0;
             this.ufo.body.acceleration.y = 0;
         }
-
     }
 }
+
+
 
 class SimpleGame {
     game:Phaser.Game;
